f06f40a160bca5f373a23db0b122826e1d2b7180
snrelgh se <snrelghmail@gmail.com>
2020-02-13 18:24:37 +1000

    add drumpad buttons to drum machine

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//component for Drum Machine's display screen
+
+class Display extends React.Component {
+
+  render() {
+    return (
+        <div id="display">display</div>
+    );
+  }
+}
+
+export default Display;
